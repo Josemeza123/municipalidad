5825566e40f788ec7b124d3d146a92a01e354bed
Jose Meza <enriquesais123@gmail.com>
2024-06-12 22:38:38 -0500

    imagen

--- dashboard/src/app/layout.jsx
+++ dashboard/src/app/layout.jsx
@@ -12,14 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div style={{ display: "flex" }}>
-          <div style={{ flex: "20%" }}>
-            <SideBar />
-          </div>
-          <div style={{ flex:"80%" }}>{children}</div>
-        </div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
